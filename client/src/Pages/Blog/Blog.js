41e4ca28bc81b0590b78da7b951e29fb44977699
austinratcliff <hello@austinratcliff.com>
2019-09-24 17:04:00 -0500

    Add Blog component with styles

--- client/src/Pages/Blog/Blog.js
+++ client/src/Pages/Blog/Blog.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import './Blog.css'
 
 const Blog = () => {
-  return <div className="Blog"></div>
+  return (
+    <div className="Blog">
+      <h1 className="header">
+        <i
+          className="fas fa-align-left"
+          title="blog"
+        /> Blog
+      </h1>
+      <i
+        className="fas fa-angle-left fa-2x home"
+        title="home"
+      />
+    </div>
+  )
 }
 
 export default Blog
