8dbe4cf89f8aa5e4209b06afd7d75624dcc618df
austinratcliff <hello@austinratcliff.com>
2019-09-24 16:42:39 -0500

    Add About component with styles

--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import './About.css'
 
 const About = () => {
-  return <div className="About"></div>
+  return (
+    <div className="About">
+      <h1 className="header">
+        <i
+          className="fas fa-user"
+          title="about"
+        /> About
+      </h1>
+      <i
+        className="fas fa-angle-up fa-2x home"
+        title="home"
+      />
+    </div>
+  )
 }
 
 export default About
