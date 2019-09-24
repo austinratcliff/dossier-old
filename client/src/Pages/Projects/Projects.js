59a4c57066fe545ef8f498d4bad5eddd1e9622df
austinratcliff <hello@austinratcliff.com>
2019-09-24 17:50:27 -0500

    Add Projects component with styles

--- client/src/Pages/Projects/Projects.js
+++ client/src/Pages/Projects/Projects.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import './Projects.css'
 
 const Projects = () => {
-  return <div className="Projects"></div>
+  return (
+    <div className="Projects">
+      <h1 className="header">
+        <i
+          className="fas fa-code"
+          title="projects"
+        /> Projects
+      </h1>
+      <i
+        className="fas fa-angle-right fa-2x home"
+        title="home"
+      />
+    </div>
+  )
 }
 
 export default Projects
