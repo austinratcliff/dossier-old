c8fc8571037408dd1e38799c61ceebe84a0b8b7a
austinratcliff <hello@austinratcliff.com>
2019-09-24 17:27:10 -0500

    Add Contact component with styles

--- client/src/Pages/Contact/Contact.js
+++ client/src/Pages/Contact/Contact.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import './Contact.css'
 
 const Contact = () => {
-  return <div className="Contact"></div>
+  return (
+    <div className="Contact">
+      <h1 className="header">
+        <i
+          className="fas fa-comment-dots"
+          title="contact"
+        /> Contact
+      </h1>
+      <i
+        className="fas fa-angle-down fa-2x home"
+        title="home"
+      />
+    </div>
+  )
 }
 
 export default Contact
