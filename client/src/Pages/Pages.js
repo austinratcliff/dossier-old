98a68a5a61a72d4369bc0fb7a8d06596e7cfabeb
austinratcliff <hello@austinratcliff.com>
2019-09-24 20:43:23 -0500

    Add CSS Transitions to Pages component

--- client/src/Pages/Pages.js
+++ client/src/Pages/Pages.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { CSSTransition } from 'react-transition-group'
 import Home from './Home/Home'
 import About from './About/About'
 import Blog from './Blog/Blog'
@@ -34,43 +35,44 @@ class Pages extends Component {
     })
   }
 
-  setPage() {
-    const pages = {...this.state.pages}
-    let pageNameToSet
-    let pageToSet
-
-    for (const page in pages) {
-      if (pages[page]) {
-        pageNameToSet = page
-      }
-    }
-
-    switch (pageNameToSet) {
-      case 'home':
-        pageToSet = <Home handleClick={this.handleClick} />
-        break
-      case 'about':
-        pageToSet = <About handleClick={this.handleClick} />
-        break
-      case 'blog':
-        pageToSet = <Blog handleClick={this.handleClick} />
-        break
-      case 'contact':
-        pageToSet = <Contact handleClick={this.handleClick} />
-        break
-      case 'projects':
-        pageToSet = <Projects handleClick={this.handleClick} />
-        break
-      default:
-        pageToSet = null
-    }
-
-    return pageToSet
-  }
-
   render() {
-    const page = this.setPage()
-    return <div className="Pages">{page}</div>
+    return (
+      <div className="Pages">
+        <Home handleClick={this.handleClick} />
+        <CSSTransition
+          classNames="about"
+          in={this.state.pages.about}
+          timeout={1000}
+          unmountOnExit
+        >
+          <About handleClick={this.handleClick} />
+        </CSSTransition>
+        <CSSTransition
+          classNames="blog"
+          in={this.state.pages.blog}
+          timeout={1000}
+          unmountOnExit
+        >
+          <Blog handleClick={this.handleClick} />
+        </CSSTransition>
+        <CSSTransition
+          classNames="contact"
+          in={this.state.pages.contact}
+          timeout={1000}
+          unmountOnExit
+        >
+          <Contact handleClick={this.handleClick} />
+        </CSSTransition>
+        <CSSTransition
+          classNames="projects"
+          in={this.state.pages.projects}
+          timeout={1000}
+          unmountOnExit
+        >
+          <Projects handleClick={this.handleClick} />
+        </CSSTransition>
+      </div>
+    )
   }
 }
 
