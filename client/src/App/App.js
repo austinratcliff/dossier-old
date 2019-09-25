ac7d7ac300b9353b58f5ffc3ffa81ebd3d191473
austinratcliff <hello@austinratcliff.com>
2019-09-24 21:01:57 -0500

    Add Pages to App component

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,36 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import logo from '../logo.svg'
+import React from 'react'
+import Pages from '../Pages/Pages'
 import './App.css'
 
 const App = () => {
-  const [tests, setTests] = useState([])
-
-  useEffect(() => {
-    fetch('/api/test')
-      .then(res => res.json())
-      .then(tests => setTests(tests))
-      .catch(err => console.log(err))
-  }, [])
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {
-          tests.map(test => <p key={test._id}>{test.test}</p>)
-        }
-      </header>
+      <Pages />
     </div>
   )
 }
