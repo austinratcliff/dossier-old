c2f8926b4c5fdafc875c942db9cf6ec054d98799
austinratcliff <hello@austinratcliff.com>
2019-09-25 20:46:01 -0500

    Add Dev.to API to Blog component

--- client/src/Pages/Blog/Blog.js
+++ client/src/Pages/Blog/Blog.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Blog.css'
 
 const Blog = props => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_BLOG_URL, {
+      headers: {
+        'api_key': process.env.REACT_APP_BLOG_API_KEY
+      }
+    })
+      .then(res => res.json())
+      .then(posts => setPosts(posts))
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="Blog">
       <h1 className="header">
@@ -10,6 +23,15 @@ const Blog = props => {
           title="blog"
         /> Blog
       </h1>
+      {
+        posts.length > 0
+          ? posts.map(post => <p key={post.id}>{post.title}</p>).slice(0, 5)
+          : <h2 className="zero-posts">
+              <span className="highlight">
+                No posts yet...
+              </span>
+            </h2>
+      }
       <i
         className="fas fa-angle-left fa-2x home"
         title="home"
