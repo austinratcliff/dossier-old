423d574cbea5f37dae569224b59933b29e4f2fc3
austinratcliff <hello@austinratcliff.com>
2019-09-26 22:45:42 -0500

    Add Routes for Pages and Admin components to App component

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react'
 import Pages from '../Pages/Pages'
+import Admin from '../Admin/Admin'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   return (
     <div className="App">
-      <Pages />
+      <Router>
+        <Security
+          issuer={process.env.REACT_APP_OKTA_ORG_URL + '/oauth2/default'}
+          clientId={process.env.REACT_APP_OKTA_CLIENT_ID}
+          redirectUri={window.location.origin + '/implicit/callback'}
+        >
+          <Route
+            path='/'
+            exact={true}
+            component={Pages}
+          />
+          <SecureRoute
+            path='/admin'
+            component={Admin}
+          />
+          <Route
+            path='/implicit/callback'
+            component={ImplicitCallback}
+          />
+        </Security>
+      </Router>
     </div>
   )
 }
