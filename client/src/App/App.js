d424d0fa78f1642a7432fe5abf8bed91326d626a
austinratcliff <hello@austinratcliff.com>
2019-11-15 12:48:07 -0600

    Remove OKTA React route due to config error during Heroku deploy

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react'
 import Pages from '../Pages/Pages'
-import Admin from '../Admin/Admin'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -10,25 +8,11 @@ const App = () => {
   return (
     <div className="App">
       <Router>
-        <Security
-          issuer={process.env.REACT_APP_OKTA_ORG_URL + '/oauth2/default'}
-          clientId={process.env.REACT_APP_OKTA_CLIENT_ID}
-          redirectUri={window.location.origin + '/implicit/callback'}
-        >
-          <Route
-            path='/'
-            exact={true}
-            component={Pages}
-          />
-          <SecureRoute
-            path='/admin'
-            component={Admin}
-          />
-          <Route
-            path='/implicit/callback'
-            component={ImplicitCallback}
-          />
-        </Security>
+        <Route
+          path='/'
+          exact={true}
+          component={Pages}
+        />
       </Router>
     </div>
   )
